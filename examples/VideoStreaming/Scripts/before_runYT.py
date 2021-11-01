c5bbf0d9142116186838b5808403bb47bd855a82
brianlochanan <brianlochan@hotmail.com>
2021-11-01 10:46:58 +0100

    setup twitch

--- examples/VideoStreaming/Scripts/before_runYT.py
+++ examples/VideoStreaming/Scripts/before_runYT.py
@@ -5,12 +5,11 @@ from AndroidRunner.Device import Device
 
 def main(device, *args, **kwargs):
 
-
     with open('/media/external/android-runner/examples/VideoStreaming/state.txt') as f:
         first_line = f.readline()
     print(first_line)
 
-    video_twitch(device, "360p", first_line)
+    video_youtube(device, "1080p", first_line)
 
 
     with open('/media/external/android-runner/examples/VideoStreaming/state.txt', 'r') as fin:
@@ -25,48 +24,40 @@ def tap(device: Device, x: int, y: int, sleep = 0) -> None:
     # The time to update is vary.
     time.sleep(sleep)
 
-def video_twitch(device, quality, url):
+def video_youtube(device, quality, url):
     os.system("echo Hello from the other side!")
     os.system("adb shell am start -a android.intent.action.VIEW '"+url+"'")
 
-
-    
     default_wait_time = 4
 
-    # tap(device, 627, 354, default_wait_time + 3)
+    tap(device, 627, 354, default_wait_time + 3)
 
-    tap(device, 670, 88, default_wait_time + 40)
-    tap(device, 670, 88)
-    tap(device, 670, 88)
+    tap(device, 627, 354, default_wait_time + 30)
 
-    # tap(device, 627, 354, default_wait_time)
+    tap(device, 627, 354, default_wait_time)
 
-    # tap(device, 348, 226, default_wait_time + 5)
+    tap(device, 348, 226, default_wait_time + 5)
 
-    # # click ____
-    # # tap(device, 348, 226, 35)
+    # click ____
+    tap(device, 348, 226)
 
-    # # click ____ this takes longer to load, wait more time
-    # tap(device, 712, 96, 1)
-    # tap(device, 155, 810, default_wait_time)
-    # tap(device, 186, 1069, default_wait_time)
+    # click ____ this takes longer to load, wait more time
+    tap(device, 712, 96, default_wait_time)
+    tap(device, 155, 810, default_wait_time)
+    tap(device, 186, 1069, default_wait_time)
 
     if quality == "360p":
         #320p
-        tap(device, 85, 637, default_wait_time)
-        tap(device, 650, 1232, default_wait_time)
+        tap(device, 162, 922, default_wait_time)
 
     if quality == "480p":
         # 480p
-        tap(device, 78, 554, default_wait_time)
-        tap(device, 650, 1232, default_wait_time)
+        tap(device, 146, 850, default_wait_time)
 
     if quality == "720p":
         #720p
-        tap(device, 132, 408, default_wait_time)
-        tap(device, 650, 1232, default_wait_time)
+        tap(device, 164, 765, default_wait_time)
 
     if quality == "1080p":
         ##1080p
-        tap(device, 123, 344, default_wait_time)
-        tap(device, 650, 1232, default_wait_time)
+        tap(device, 173, 680, default_wait_time)
